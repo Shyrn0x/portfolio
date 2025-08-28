be2a053b1e8d0c54d15bdd8f3819fe50b2d746e6
shyrn0x <ikyozy@gmail.com>
2025-08-28 15:14:58 +0700

    Speed insights fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 import { Geist, Geist_Mono } from "next/font/google";
-import { SpeedInsights } from '@vercel/speed-insights/next';
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import "./globals.css";
 
 const geistSans = Geist({
